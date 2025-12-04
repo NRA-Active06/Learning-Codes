39d0259b4c435c35f1ff7796b3b15f7b65cac9e6
Noor Amini <Noor.amini@arm.com>
2025-12-04 09:55:38 +0000

    advent of code stuff

---
+++ adventOfCode/day1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main() {
+    int position = 50;
+    int target = 0;
+    std::string line;
+
+    std::ifstream in("day1.txt");
+    if (!in) {
+        std::cerr << "Could not open the txt\n";
+        return 1;
+    }
+
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) {
+            continue;
+        }
+    
+        char dir = line[0];
+        int cycle = std::stoi(line.substr(1));
+        cycle %= 100;
+
+        if (dir == 'R') {
+            position = (position + cycle) % 100;
+        }
+        else if (dir == 'L') {
+            position = (position - cycle + 100) % 100;
+        }
+        else {
+            std::cerr << "Invalid input: \n";
+            return 1;
+        }
+
+        if (position == 0) {
+            target++;
+        }
+    }
+    std::cout << "dial zeroes is: " << target << std::endl;
+    return 0;
+}
